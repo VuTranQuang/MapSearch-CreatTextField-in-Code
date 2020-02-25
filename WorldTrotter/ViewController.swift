f4f64065cfeb050f70d966bfb9f84ca10f4ff620
TranQuangVu <vutran7993@gmail.com>
2020-02-25 14:15:56 +0700

    Customs Textfield lengths and take only integer

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    static let share = ViewController()
+    var maxLengths = [UITextField: Int]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textField.delegate = self
+        
     }
-
-
+    // MARK: TextField take only Integer
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let s = NSString(string: textField.text ?? "").replacingCharacters(in: range, with: string)
+        guard !s.isEmpty else {
+            return true
+        }
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .none
+        return numberFormatter.number(from: s)?.intValue != nil
+    }
 }
 
+// MARK: Customs textField Maxlength
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = ViewController.share.maxLengths[self] else {
+                return 150
+            }
+            return l
+        }
+        set {
+            ViewController.share.maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix(textField:)), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        if let t = textField.text {
+            textField.text = String(t.prefix(maxLength))
+        }
+    }
+}
